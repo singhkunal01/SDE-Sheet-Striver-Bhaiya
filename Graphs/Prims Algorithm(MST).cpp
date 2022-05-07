36f87ea9a6d2a4e086f8018a65178053c12b43db
Kunal Singh <kunalsingh142001@gmail.com>
2022-05-08 01:16:28 +0530

    Prims Algorithm.cpp

--- Graphs/Prims Algorithm(MST).cpp
+++ Graphs/Prims Algorithm(MST).cpp
@@ -24,7 +24,7 @@ vector<int> primsAlgorithm(int vert, vector<pair<int, int>> adj[]) {
 	for (int i = 0; i < vert - 1 ; i++) {
 		int mini = INT_MAX, u;
 		for (int v = 0; v < vert; v++) {
-			if (mstSet[v] == 0 and key[v] < mini) {
+			if (mstSet[v] == false and key[v] < mini) {
 				mini = key[v], u = v;
 			}
 		}
@@ -35,6 +35,33 @@ vector<int> primsAlgorithm(int vert, vector<pair<int, int>> adj[]) {
 			int weight = it.second, node = it.first;
 			if (mstSet[node] == false and weight < key[node]) {
 				parent[node] = u, key[node] = weight;
+			}
+		}
+	}
+	return key;
+}
+
+
+//2. Efficient Approach using Priority Queue
+vector<int> primsAlgorithmEfficient(int vert, vector<pair<int, int>> adj[]) {
+	vector<int> key(vert, INT_MAX), mstSet(vert, 0), parent(vert, -1);
+//default
+	key[0] = 0;
+	priority_queue < pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+	pq.push({0, 0});
+//total number of edges in MST are vertices - 1 so ,
+	for (int i = 0; i < vert - 1 ; i++) {
+//instead of findint minimum among all keys we use priority_queues of min heap so that everytime we get the min key in (logn)
+		int u = pq.top().second;
+		pq.pop();
+		//mark the node as visited or the part of mst
+		mstSet[u] = 1;
+		//check or all adjacents of that current node
+		for (auto &it : adj[u]) {
+			int weight = it.second, node = it.first;
+			if (mstSet[node] == false and weight < key[node]) {
+				parent[node] = u, key[node] = weight;
+				pq.push({key[node], node});//here first key is the min weight so that we sort accordingly and second is the node number
 			}
 		}
 	}
